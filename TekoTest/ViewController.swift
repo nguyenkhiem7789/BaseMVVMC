ab4337ab0d1073e4f0ffbd07dfa050a1f7b32da2
Khiem Nguyen <nguyenkhiem7789@gmail.com>
2020-05-21 18:03:11 +0700

    create project

---
+++ TekoTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TekoTest
+//
+//  Created by Nguyen on 5/21/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
